4b1c876b678c5629499a8de69f31a64a92deba2c
Сом <117897751+ILYA222213@users.noreply.github.com>
2023-12-18 11:39:35 +0300

    Update Program.cs

--- image/image/Program.cs
+++ image/image/Program.cs
@@ -2,18 +2,18 @@
 
 internal class Program
 {
-    private static async Task Main(string[] args)
+    private static async Task Main(string args)
     {
-        HttpClient client = new();
+        HttpClient client = new(); // Создание экземпляра HttpClient
 
         Console.WriteLine("Введите ссылку для скачивания картинки: ");
-        string nameWebsite = Console.ReadLine();
+        string nameWebsite = Console.ReadLine(); // Ввод пользователем ссылки для скачивания картинки
 
-        HttpResponseMessage response = await client.GetAsync(nameWebsite);
-        byte[] data = await response.Content.ReadAsByteArrayAsync();
+        HttpResponseMessage response = await client.GetAsync(nameWebsite); // Выполнение HTTP GET запроса для получения картинки
+        byte data = await response.Content.ReadAsByteArrayAsync(); // Чтение данных картинки в виде массива байтов
 
-        await File.WriteAllBytesAsync("C:\\Users\\ilruz\\OneDrive\\Рабочий стол\\image.jpg", data);
+        await File.WriteAllBytesAsync("C:\\Users\\ilruz\\OneDrive\\Рабочий стол\\image.jpg", data); // Сохранение картинки на жесткий диск
 
-        Process.Start(new ProcessStartInfo { FileName = "C:\\Users\\ilruz\\OneDrive\\Рабочий стол\\image.jpg", UseShellExecute = true });
+        Process.Start(new ProcessStartInfo { FileName = "C:\\Users\\ilruz\\OneDrive\\Рабочий стол\\image.jpg", UseShellExecute = true }); // Открытие картинки с помощью программы по умолчанию
     }
 }
